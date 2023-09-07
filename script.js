127af768fc11847c07b2832c3b2d096373bdee2f
Pauline Muigai <wanjirumuigai@gmail.com>
2023-09-07 10:50:46 +0300

    created html, css and js pages

---
+++ script.js
@@ -0,0 +1,12 @@
+var date = new Date();
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const day = weekday[date.getDay()];
+document.getElementById("txtStartDate").innerHTML = day;
