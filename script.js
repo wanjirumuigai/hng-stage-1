3075fd7a40e5202870c64b695299ba07656afc1f
Pauline Muigai <wanjirumuigai@gmail.com>
2023-09-08 13:11:22 +0300

    edited the image alt

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-function dayOftheWeek() {
+// Function to update the current day of the week in Nigeria (WAT time zone)
+function updateCurrentDayOfTheWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   const currentDate = new Date();
-  const weekdays = { weekday: "long", timeZone: "Africa/Lagos" };
-  const currentDay = new Intl.DateTimeFormat("en-US", weekdays).format(
+  const options = { weekday: "long", timeZone: "Africa/Lagos" }; // Nigeria's time zone
+  const currentDay = new Intl.DateTimeFormat("en-US", options).format(
     currentDate
   );
-  document.querySelector(
-    '[data-testid="currentDayOfTheWeek"]'
-  ).textContent = `Current Day: ${currentDay}`;
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    currentDay;
 }
 
-dayOftheWeek();
+// Initial update
+updateCurrentDayOfTheWeek();
 
-setInterval(dayOftheWeek, 86400000);
+// Update the current day of the week every 24 hours
+setInterval(updateCurrentDayOfTheWeek, 86400000);
 
-function updateLocalTime() {
+// Function to update the current local time in Nigeria (WAT time zone)
+function updateCurrentLocalTime() {
   const currentDate = new Date();
-  const weekdays = { timeZone: "Africa/Lagos" };
-  const localTime = currentDate.toLocaleTimeString("en-US", weekdays);
-  const utcTimeMs = currentDate.getTime(); // Get UTC time in milliseconds
-  const output =
-    "Local Time: " + localTime + "<br>" + "UTC Time (ms): " + utcTimeMs;
-  document.querySelector('[data-testid="currentUTCTime"]').innerHTML = output;
+  const options = { timeZone: "Africa/Lagos" }; // Nigeria's time zone
+  const localTime = currentDate.toLocaleTimeString("en-US", options);
+  const utcTimeMilliseconds = currentDate.getTime(); // Get UTC time in milliseconds
+  document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    "Local Time: " + localTime + " | UTC Time (ms): " + utcTimeMilliseconds;
 }
 
-updateLocalTime();
+// Initial update
+updateCurrentLocalTime();
 
-// Update the current local time
-setInterval(updateLocalTime, 1000);
+// Update the current local time every second
+setInterval(updateCurrentLocalTime, 1000);
